5a4748d84252c6881eb3841ad46b5c59318bf49b
Piyush Pankhania <piyushpankhania@gmail.com>
2023-02-17 10:07:47 +0530

    Footer Page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
- 
+import Footer from "./components/Footer";
+import Videos from "./components/Videos";
 function App() {
   return (
      <Router> 
      <Header />
       <Routes>
          <Route path='/' element={<Home />} />
+         <Route path='/videos' element={<Videos />} />
       </Routes>
+      <Footer/>
      </Router>
   );
 }
