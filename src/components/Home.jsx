f50a7b817e0e0d57bece3d4dd4799f65a105711b
Piyush Pankhania <piyushpankhania@gmail.com>
2023-02-17 10:06:43 +0530

    Footer Page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Box, Image, Heading, Container } from '@chakra-ui/react';
+import { Box, Image, Heading, Container, Stack,Text } from '@chakra-ui/react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import img1 from '../assets/1.jpg';
 import img2 from '../assets/2.jpg';
 import img3 from '../assets/3.jpg';
 import img4 from '../assets/4.jpg';
-// import img5 from '../assets/5.png';
+import img5 from '../assets/5.png';
 const headingOptions = {
   pos: 'absolute',
   left: '50%',
@@ -20,9 +20,29 @@ const Home = () => {
   return (
     <Box>
       <MyCarousel />
-      <Container minH={"100vh"} maxW={"container.xl"} p="16">
-        <Heading textTransform={"uppercase"} py="2" w={"fit-content"} borderBottom={"2px solid"} m="auto">Services</Heading>
+      <Container minH={'100vh'} maxW={'container.xl'} p="16">
+        <Heading
+          textTransform={'uppercase'}
+          py="2"
+          w={'fit-content'}
+          borderBottom={'2px solid'}
+          m="auto"
+        >
+          Services
+        </Heading>
+        <Stack
+          h="full"
+          p={'4'}
+          alignItems={'center'}
+          direction={['column', 'row']}
+        >
+          <Image src={img5} h={["40","400"]} filter={'hue-rotate(-130deg)'} />
+          <Text letterSpacing={"widest"} lineHeight= {"190%"} p={["4","16"]} textAlign="center">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 
+
+          </Text>
+        </Stack>
       </Container>
     </Box>
   );
